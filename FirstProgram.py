89de7f25d8a7a54b10ac1802e3fb923e769e26a2
vermapiyush823 <vermapiyush823@gmail.com>
2024-05-02 09:20:25 +0530

    commit

--- FirstProgram.py
+++ FirstProgram.py
@@ -3,6 +3,43 @@
 # print("I am", age, "years old")
 
 
-num1 = 10
-num2 = 15
-print(num1+num2)
+# num1 = 10
+# num2 = 15
+# print(num1*"Q")
+
+# print("num1"+"Q")
+
+
+# Integer Division
+# a = 10
+# b = 3
+# print(a//b)
+
+# Float Division
+# a = 10
+# b = 3
+# print(a/b)
+
+
+
+# Taking input from user
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# print("Hello", name, "you are", age, "years old")
+
+
+
+# Conditional Statements
+# age = int(input("Enter your age: "))
+# if(age>18):
+#     print("You are an adult")
+# elif(age>12 and age<=18):
+#     print("You are a teenager")
+# else:
+#     print("You are a child")
+
+
+# Single Line ternary operator
+food = input("Enter your favourite food: ")
+eat = "yes" if food == "pizza" else "no"
+print(eat)
